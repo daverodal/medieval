65ee3f7ca3f7f9c396469006d169c6611c9fec33
david <dave.rodal@gmail.com>
2018-09-22 10:47:37 -0400

    img tags now fetch with protocol-less urls

--- Medieval/angular-view.blade.php
+++ Medieval/angular-view.blade.php
@@ -239,7 +239,7 @@
                                 </defs>
                             </svg>
                         </div>
-                        <img id="map" alt="map" src="<?php preg_match("/http/",$mapUrl) ?   $pre = '': $pre = url('.');echo "$pre$mapUrl";?>">
+                        <img id="map" alt="map" src="<?php preg_match("/http/",$mapUrl) ?   $pre = '': $pre = url('.'); $mapUrl = preg_replace("/http[s]?:/", "", $mapUrl); echo "$pre$mapUrl";?>">
                         <?php $id = 0; ?>
 
                         @section('units')
